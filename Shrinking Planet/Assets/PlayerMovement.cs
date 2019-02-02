f44e807bc3314753b490d6484791c4e56353e3ef
Trey <ragegiraffe@gmail.com>
2019-02-02 02:13:31 -0600

    e

--- Shrinking Planet/Assets/PlayerMovement.cs
+++ Shrinking Planet/Assets/PlayerMovement.cs
@@ -14,14 +14,16 @@ public class PlayerMovement : MonoBehaviour
         transform.position += transform.forward * speed * Time.deltaTime;
 
         Debug.DrawRay(transform.position, transform.forward * 5, Color.red);
+
+        AdjustPosition();
     }
 
     public void AdjustPosition()
     {
-        LayerMask mask = LayerMask.GetMask("Planet");
-        if (Physics.Raycast(transform.position, -transform.up, .1f, mask))
+        //LayerMask mask = LayerMask.GetMask("Planet");
+        if (!Physics.Raycast(transform.position - new Vector3(0f,GetComponent<BoxCollider>().bounds.extents.y), -transform.up, .1f))
         {
-
+            transform.position += -transform.up * .1f;
         }
     }
 }
