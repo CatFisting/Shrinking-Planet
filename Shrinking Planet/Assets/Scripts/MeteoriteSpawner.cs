21186b4b716c5443bd54dd6b00b35ab2718fb0d6
pixelfox01 <47266961+pixelfox01@users.noreply.github.com>
2019-02-02 13:37:28 +0530

    Created meteorite spawner

---
+++ Shrinking Planet/Assets/Scripts/MeteoriteSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteoriteSpawner : MonoBehaviour
+{
+
+    [SerializeField] GameObject meteoritePrefab;
+    [SerializeField] GameObject planet;
+
+    bool spawning = true;
+
+    [SerializeField] float spawnerRadius = 15f;
+    [SerializeField] float meteoriteSpeed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnMeteorite());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator SpawnMeteorite() {
+        while (spawning) {
+            Vector3 targetPos = Random.onUnitSphere * spawnerRadius;
+            GameObject meteorite = Instantiate(this.meteoritePrefab, targetPos, Quaternion.identity);
+            Vector3 direction = planet.transform.position - meteorite.transform.position;
+            meteorite.transform.forward = direction;
+            Rigidbody rb = meteorite.GetComponent<Rigidbody>();
+            rb.velocity = direction * meteoriteSpeed;
+            yield return new WaitForSeconds(3f);
+        }
+    }
+}
---
+++ Shrinking Planet/Assets/Scripts/SpawnCrater.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCrater : MonoBehaviour
+{
+    [SerializeField] GameObject craterPrefab;
+    [SerializeField] GameObject planet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Planet")) {
+            GameObject crater = Instantiate(craterPrefab, collision.GetContact(0).point, Quaternion.identity);
+            crater.transform.forward = collision.GetContact(0).normal;
+            Vector3 direction = crater.transform.position - planet.transform.position;
+            crater.transform.position = direction * 0.1f;
+            Destroy(gameObject);
+        }
+    }
+}
