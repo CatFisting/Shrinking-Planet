f09cf392f720c550c4a16486a18dbe0c33d0aee5
Trey <ragegiraffe@gmail.com>
2019-02-02 01:24:18 -0600

    changed car prefab

---
+++ Shrinking Planet/Assets/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject planet;
+    public float speed = 10f;
+
+    void Update()
+    {
+        transform.up = -(planet.transform.position - transform.position).normalized;
+
+        transform.position += -transform.right * speed * Time.deltaTime;
+    }
+}
